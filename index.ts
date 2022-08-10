012f34f975d966bb00706d87fa12f9c28ca9ea8c
Yakovlev Kll <yakovlev.kll@gmail.com>
2022-08-10 09:52:03 +0000

    fix(DTO): :bug: Fixing types

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
 /* TYPES */
 
-import { ResponseStatus } from './frontend/typings'
-import { UUID } from './typings'
-
-import { Editor2dProtocol } from './frontend/typings'
-
-export type { Editor2dProtocol, UUID, ResponseStatus }
-
 /* ELSE */
 
 import { Editor2dDescriptor } from './frontend/2d-editor'
