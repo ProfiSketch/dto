441165e626d74ff53888857c7c05ef63fb867e21
Yakovlev Kll <yakovlev.kll@gmail.com>
2022-08-10 10:04:30 +0000

    fix(DTO): :bug: Typings export fix

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 /* TYPES */
 
+import { Editor2dProtocol } from './frontend/typings'
+import { UUID } from './typings'
+import { ResponseStatus } from './frontend/typings'
+
+export { Editor2dProtocol, UUID, ResponseStatus }
+
 /* ELSE */
 
 import { Editor2dDescriptor } from './frontend/2d-editor'
