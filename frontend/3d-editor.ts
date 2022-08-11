43e5f65fefad082623ada8d54cd7ed6227e3ad4c
Yakovlev Kll <yakovlev.kll@gmail.com>
2022-08-11 18:56:34 +0000

    feat(DTO): :sparkles: Editor3d protocol and descriptor provided

---
+++ frontend/3d-editor.ts
@@ -0,0 +1,9 @@
+import { ProtoframeDescriptor } from 'protoframe'
+
+import { Editor3dProtocol } from './typings'
+
+const Editor3dDescriptor: ProtoframeDescriptor<Editor3dProtocol> = {
+  type: 'editor3d',
+}
+
+export { Editor3dDescriptor }
--- frontend/typings.ts
+++ frontend/typings.ts
@@ -41,3 +41,49 @@ export interface Editor2dProtocol extends Protoframe {
     body: { ids: UUID[] }
   }
 }
+
+export type File3d = { blob: Blob; mime: MimeType3d; filename: string }
+
+export interface Editor3dProtocol extends Protoframe {
+  /* Tasks */
+
+  loadScene: {
+    body: { file: File3d }
+    response: RequestResponse
+  }
+  loadObjects: {
+    body: { file: File3d; id: UUID }
+    response: RequestResponse
+  }
+  selectObjects: {
+    body: { ids: UUID[] }
+    response: RequestResponse
+  }
+  deleteObjects: {
+    body: { ids: UUID[] }
+    response: RequestResponse
+  }
+  updateObjectUuid: {
+    body: { from: string; to: UUID }
+    response: RequestResponse
+  }
+
+  /* Events */
+
+  objectIsAdded: {
+    body: { id: UUID }
+  }
+  objectsAreSelected: {
+    body: { ids: UUID[] }
+  }
+  //   TODO: implement later
+  //   objectsAreConnected: {
+  //     body: { elems: { id: UUID; port: SchemeElemPort }[] }
+  //   }
+  objectsAreDeleted: {
+    body: { ids: UUID[] }
+  }
+  objectsAreRestored: {
+    body: { ids: UUID[] }
+  }
+}
