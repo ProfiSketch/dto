9e1ef7ea527976b517215e58d5c02d0c99ec2156
Yakovlev Kll <yakovlev.kll@gmail.com>
2022-08-11 18:57:09 +0000

    feat(DTO): :sparkles: Module exports updated

--- index.ts
+++ index.ts
@@ -1,13 +1,45 @@
-/* TYPES */
+/* General */
 
-import { Editor2dProtocol } from './frontend/typings'
 import { UUID } from './typings'
 import { ResponseStatus } from './frontend/typings'
 
-export { Editor2dProtocol, UUID, ResponseStatus }
+export { UUID, ResponseStatus }
 
-/* ELSE */
+/* Mime types */
+
+import {
+  FileExtension2d,
+  FileExtension3d,
+  MimeType2d,
+  MimeType3d,
+} from './frontend/mime'
+
+export { FileExtension2d, FileExtension3d, MimeType2d, MimeType3d }
+
+import {
+  FileExtensionMimeType2d_Map,
+  FileExtensionMimeType3d_Map,
+  MimeTypeFileExtension2d_Map,
+  MimeTypeFileExtension3d_Map,
+  MimeTypes2d_Map,
+  MimeTypes3d_Map,
+} from './frontend/mime'
+
+export {
+  FileExtensionMimeType2d_Map,
+  FileExtensionMimeType3d_Map,
+  MimeTypeFileExtension2d_Map,
+  MimeTypeFileExtension3d_Map,
+  MimeTypes2d_Map,
+  MimeTypes3d_Map,
+}
+
+/* Protoframe protocols and descriptors */
+
+import { Editor2dProtocol, Editor3dProtocol } from './frontend/typings'
+export { Editor2dProtocol, Editor3dProtocol }
 
 import { Editor2dDescriptor } from './frontend/2d-editor'
+import { Editor3dDescriptor } from './frontend/3d-editor'
 
-export { Editor2dDescriptor }
+export { Editor2dDescriptor, Editor3dDescriptor }
