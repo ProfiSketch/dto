5d61e0159610755eb1a5a183e2a1a9b0105bab5f
Yakovlev Kll <yakovlev.kll@gmail.com>
2022-08-11 01:33:16 +0000

    feat(DTO): :sparkles: Editor2dProtocol updated

--- frontend/typings.ts
+++ frontend/typings.ts
@@ -3,12 +3,37 @@ import { UUID } from '../typings'
 
 export type ResponseStatus = 'success' | 'warning' | 'error'
 
+type RequestResponse = { status: ResponseStatus; description?: string }
+
+export type SchemeElemPort = string | number
+
 export interface Editor2dProtocol extends Protoframe {
-  setCellsSelected: {
+  /* Requests */
+  selectElems: {
     body: { ids: UUID[] }
-    response: { status: ResponseStatus; description: string }
+    response: RequestResponse
   }
-  cellsAreSelected: {
+  deleteElems: {
+    body: { ids: UUID[] }
+    response: RequestResponse
+  }
+  updateElemUuid: {
+    body: { from: string; to: UUID }
+    response: RequestResponse
+  }
+
+  /* Events */
+  elemIsAdded: {
+    body: { ports: SchemeElemPort[] }
+  }
+  elemsAreSelected: {
     body: { ids: UUID[] }
   }
+  elemsAreConnected: {
+    body: { elems: { id: UUID; port: SchemeElemPort }[] }
+  }
+  elemsAreDeleted: {
+    body: { ids: UUID[] }
+  }
+  //   What about Ctrl + Z ?
 }
