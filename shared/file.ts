b508cd05d14ff1ceecc3e8f595ef5871bf7b0414
Yakovlev Kll <yakovlev.kll@gmail.com>
2022-10-17 14:27:49 +0000

    fix(Shared types): :ambulance: File2d and File3d fixed

--- shared/file.ts
+++ shared/file.ts
@@ -3,13 +3,11 @@ import { MimeType2d, MimeType3d } from './mime'
 export type File2dContent = Blob
 
 export interface File2d extends File {
-  mime: MimeType2d
-  blob: File2dContent
+  type: MimeType2d
 }
 
 export type File3dContent = Blob
 
 export interface File3d extends File {
-  mime: MimeType3d
-  blob: File2dContent
+  type: MimeType3d
 }
